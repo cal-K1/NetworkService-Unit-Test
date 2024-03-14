93d5f5fd93fa094301fd68ca7e660368d0bd0687
Callum Cunningham <ccunningham@redev.co.uk>
2024-03-14 10:08:57 +0000

    Added tests for different variable types including Objects and IEnumerable

--- Network Utility Unit Test Example/Ping/Network Service.cs
+++ Network Utility Unit Test Example/Ping/Network Service.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -18,6 +19,54 @@ namespace Network_Utility_Unit_Test_Example.Ping
         public int PingTimeout(int a, int b)
         {
             return a + b;
+        }
+
+        public DateTime LastPingDate()
+        {
+            return DateTime.Now;
+        }
+
+        public PingOptions GetPingOptions()
+        {
+            // This is a random object found in a lib somewhere, not actually being used, just for example.
+            return new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1,
+            };
+        }
+
+        public IEnumerable<PingOptions> MostRecentPings()
+        {
+            // Generic IEnumerable (basically a list of objects)
+            IEnumerable<PingOptions> pingOptions = new[]
+            {
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+
+                new PingOptions()
+                {
+                    DontFragment = true,
+                    Ttl = 1
+                },
+            };
+
+            return pingOptions;
         }
     }
 }
--- NetworkUtility.Test/PingTests/NetworkServiceTests.cs
+++ NetworkUtility.Test/PingTests/NetworkServiceTests.cs
@@ -7,11 +7,23 @@ using Xunit;
 using NetworkUtility;
 using Network_Utility_Unit_Test_Example.Ping;
 using FluentAssertions;
+using Xunit.Sdk;
+using System.Net.WebSockets;
+using System.Globalization;
+using FluentAssertions.Extensions;
+using System.Net.NetworkInformation;
 
 namespace NetworkUtility.Test.PingTests
 {
     public class NetworkServiceTests
     {
+        private readonly NetworkService _pingService;
+        public NetworkServiceTests()
+        {
+            // SUT (System Under Test)
+            _pingService = new NetworkService();
+        }
+
         [Fact]
         public void NetworkService_SendPing_ReturnString()
         {
@@ -45,5 +57,58 @@ namespace NetworkUtility.Test.PingTests
             result.Should().BeGreaterThanOrEqualTo(2);
             result.Should().NotBeInRange(-10000, 0);
         }
+
+        [Fact]
+        public void NetworkService_LastPingDate_ReturnsDateTime()
+        {
+            // Arrange
+            // We dont need anything here as we made _pingService a global var in the constructor
+
+            // Act
+            var result = _pingService.LastPingDate();
+
+
+            // Assert
+            result.Should().BeAfter(1.January(2010));
+            result.Should().BeBefore(1.January(2030));
+        }
+
+        [Fact]
+        public void NetworkService_GetPingOptions_ReturnsDateTime()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.GetPingOptions();
+
+            // Assert
+            result.Should().BeOfType<PingOptions>();
+            result.Should().BeEquivalentTo(expected);
+            result.Ttl.Should().Be(1);
+        }
+
+        [Fact]
+        public void NetworkService_MostRecentPings_ReturnsObject()
+        {
+            // Arrange
+            var expected = new PingOptions()
+            {
+                DontFragment = true,
+                Ttl = 1
+            };
+
+            // Act
+            var result = _pingService.MostRecentPings();
+
+            // Assert
+            result.Should().ContainEquivalentOf(expected);
+            result.Should().Contain(x => x.DontFragment == true);
+        }
+
     }
 }
